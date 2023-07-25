1c8696d67d262511083fbdffe158dd44d34e93e3
Cayo Cutrim <cayocutrim1991@gmail.com>
2023-07-25 16:38:40 -0300

    first commit

---
+++ Backend/src/main/java/wise/book/wisebookbackend/WiseBookApplication.java
@@ -0,0 +1,13 @@
+package wise.book.wisebookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseBookApplication.class, args);
+	}
+
+}
